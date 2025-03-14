42e6549e4855ec1b29bcbd26fd7af8ec31fc7b9a
Ronald Lanton <34621887+ronaldlanton@users.noreply.github.com>
2025-03-14 14:45:27 +0000

    [Cursor] Improve project structure - Add package __init__.py

---
+++ whatsapp_profile_changer/__init__.py
@@ -0,0 +1,6 @@
+"""
+WhatsApp Profile Changer - An automated tool to change your WhatsApp Web profile picture at regular intervals.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'WhatsApp Profile Changer Team'
